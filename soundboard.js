a59ce11cd5e97c38f9208a42a75d84e11fa849d5
Ravin Williams <will3225@msu.edu>
2020-11-09 17:20:06 -0500

    Audio files not playing

--- soundboard.js
+++ soundboard.js
@@ -1,7 +1,29 @@
+  /* creates a variable, then Finds the id function of a button */ 
 var buttonElement = document.getElementById('bell')
-    buttonElement.addEventListener('click', function () {
-    console.log('I was clicked!')
+ 
+/* Listens to see if the user clicked the button.
+        Add event listener requires: the name of the event to listen for & The callback function, 
+        which JavaScript looks back to and calls whenever that event happensThe call back function*/ 
+
+    buttonElement.addEventListener('play', (event)=> {
+
+/*The callback function, which confirms that our event listener really is working, by printing 
+I was clicked! whenever we click on the button. */ 
+    console.log('Bell button was clicked!')
   })
+
+  /* */ 
+
+
 
 var buttonElementtwo = document.getElementById('coughing')
 var buttonElementthree = document.getElementById('thunder')
+
+
+
+
+
+
+
+
+ /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */ 
