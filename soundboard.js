384fc7d565656dcde34ae388ec6f9d4685d14ee9
Ravin Williams <will3225@msu.edu>
2020-11-09 20:15:49 -0500

    checking for errors in html & JS files

--- soundboard.js
+++ soundboard.js
@@ -2,60 +2,58 @@
 var bellElement = document.getElementById('bell')
 var coughElement = document.getElementById('coughing')
 var thunderElement = document.getElementById('thunder')
+
+
+  /*Plays the bell audio*/
+    function playBell(){
+         bellElement.play()}
+
+
+/*Plays the coughing audio*/
+    function playCough(){
+        coughElementtwo.play()}
+
+
+
+/*Plays the thunder audio*/ 
+    function playThunder(){
+        thunderElement.play()} 
+
+
+
+
+
+
+
  
 /* Listens to see if the user clicked the button.
         Add event listener requires: the name of the event to listen for & The callback function, 
         which JavaScript looks back to and calls whenever that event happensThe call back function*/ 
 
-    bellElement.addEventListener('click', function () { /*Plays the bell audio */
-        bellElement.play()
+  /* Plays the bell audio 
+  bellElement.addEventListener('click', function () {  
+        bellElement.play()*/
+        
 /*The callback function, which confirms that our event listener really is working, by printing 
-I was clicked! whenever we click on the button. */ 
+I was clicked! whenever we click on the button. 
         
         console.log('Bell Button was clicked!')
         
-     })
+     })*/ 
 
 
- /*Plays the coughing audio*/
+ /*Plays the coughing audio
     coughElement.addEventListener('click', function () {
         coughElementtwo.play()
         console.log('Cough Button was clicked!')
-    })
+    })*/
  
  
- /*Plays the thunder audio*/
+ /*Plays the thunder audio
     thunderElement.addEventListener('click', function () {
         thunderElement.play()
         console.log('Thunder Button was clicked!')
-    })
+    })*/
     
   
-
-  /*Plays the bell audio
-  function playBell(){
-      bellElement.play()}*/
-
-  
-  /*Plays the coughing audio
-  function playCough(){
-    coughElementtwo.play()}*/
-
-
-
-  /*Plays the thunder audio
-  function playThunder(){
-    thunderElement.play()} */ 
-
-
-
-
-
-
-
-
-
-
-
-
  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */  /* */ 
