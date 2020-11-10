5a25b5979644b90da87de0bc35f84124faf9f9da
Ravin Williams <will3225@msu.edu>
2020-11-09 19:17:49 -0500

    modified html & JS files

--- soundboard.js
+++ soundboard.js
@@ -1,29 +1,41 @@
   /* creates a variable, then Finds the id function of a button */ 
-var buttonElement = document.getElementById('bell')
+var bellElement = document.getElementById('bell')
+var coughElement = document.getElementById('coughing')
+var thunderElement = document.getElementById('thunder')
  
 /* Listens to see if the user clicked the button.
         Add event listener requires: the name of the event to listen for & The callback function, 
         which JavaScript looks back to and calls whenever that event happensThe call back function*/ 
-
     buttonElement.addEventListener('click', function () {
-
 /*The callback function, which confirms that our event listener really is working, by printing 
 I was clicked! whenever we click on the button. */ 
-    console.log('Bell button was clicked!')
-    
-    /*Plays the audio*/
-    buttonElement.play('bicycle_bell.wav')
+    console.log('Button was clicked!')
   })
 
-  
+
+ /*Plays the bell audio*/
+  function playBell(){
+      bellElement.play()
+
+  }
+  /*Plays the coughing audio*/
+  function playCough(){
+    coughElementtwo.play()
+
+}
+
+  /*Plays the thunder audio*/
+  function playThunder(){
+    thunderElement.play()
+
+}
   
 
   /* */ 
 
 
 
-var buttonElementtwo = document.getElementById('coughing')
-var buttonElementthree = document.getElementById('thunder')
+
 
 
 
